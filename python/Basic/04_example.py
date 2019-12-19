c98f3f27c70573cd4d49da5a903ebfe3811c3802
JinhanPark92 <pjh921217@gmail.com>
2019-12-19 12:02:33 +0900

    최신화

---
+++ python/Basic/04_example.py
@@ -0,0 +1,43 @@
+# print() happy hacking!
+hphk = "해피해킹"
+
+print(hphk)
+print(hphk)
+print(hphk)
+print(hphk)
+print(hphk)
+
+# 점심메뉴판!
+
+menus1 = "순남 시래기"
+menus2 = "양자강"
+menus3 = "20층..."
+
+print(menus1)
+print(menus2)
+print(menus3)
+
+# List
+menus = ["순남 시래기", "양자강", "20층..."]
+print(menus)
+print(menus[0])
+
+
+# 전화번호부 저장하기
+
+phone_nums = ["02-1111-1111", "02-2222-2222", "02-3333-3333"]
+print(phone_nums[0])
+
+# Dictionary
+
+dict_nums = { 
+    "순남 시래기" : "02-1111-1111" , 
+    "양자강" : "02-2222-2222", 
+    "20층": "02-3333-3333" 
+}
+
+# 오류 발생!
+# print(dict_nums["헤헤"])
+
+# None!
+print(dict_nums.get("희희"))
